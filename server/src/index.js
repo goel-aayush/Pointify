116ab148519dc762e5cd08d216bebb0286954316
Aayush Goel <mgoel1296@gmail.com>
2025-07-29 15:08:06 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://pointify-clinet.vercel.app/",
     credentials: true,
   })
 );
